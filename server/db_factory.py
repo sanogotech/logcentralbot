79153dd460281ee02ef2e8042d7b0761bdfc9279
SANOGOTECH <sanogotech@yahoo.fr>
2025-06-11 13:09:28 +0000

    Update db_factory.py

--- server/db_factory.py
+++ server/db_factory.py
@@ -1,1 +1,13 @@
+def get_db_handler(dbtype):
+    if dbtype == 'sqlite':
+        from db_sqlite import SQLiteHandler
+        return SQLiteHandler()
+    elif dbtype == 'mysql':
+        from db_mysql import MySQLHandler
+        return MySQLHandler()
+    elif dbtype == 'mongodb':
+        from db_mongo import MongoHandler
+        return MongoHandler()
+    else:
+        raise ValueError(f"Unsupported database type: {dbtype}")
 
